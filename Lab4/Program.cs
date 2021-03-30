ad4272c36fd4b33148d7e6f220025ab89fcccc6b
Mpkhort <giselle98@mail.ru>
2021-03-30 08:44:26 +0300

    Lab 4 Create 3 thread: first with sum, second with mul, third with menu (stop/show)

---
+++ Lab4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        public static int num = 1;
+        async static Task Main(string[] args)
+        {
+
+            Task sum_task = SumAsync();
+            Task mul_task = MulAsync();
+            Task menu_task = MenuAsync();
+            await sum_task;
+            await mul_task;
+            await menu_task;
+        }
+        static async Task SumAsync()
+        {
+                await Task.Run(() => Sum());
+        }
+        static async Task MulAsync()
+        {
+                await Task.Run(() => Mul());
+        }
+        static async Task MenuAsync()
+        {
+            await Task.Run(() => Menu());
+        }
+
+        static void Sum() 
+        {
+            while (true)
+            {
+                num += 2;
+                Thread.Sleep(2000);
+            }
+        }
+
+        //static async Task<int> Mul()
+        static void Mul()
+        {
+            while (true)
+            {
+                num *= 3;
+                Thread.Sleep(3000);
+            }
+        }
+
+        //static async Task<int> Menu()
+        static void Menu()
+        {
+            string cmd;
+            while (true)
+            {
+                Console.WriteLine("Enter STOP/SHOW:");
+                cmd = Console.ReadLine();
+                if (cmd == "STOP" || cmd == "stop")
+                {
+                    Console.WriteLine("Closing programm...");
+                    Environment.Exit(0);
+                }
+                else if (cmd == "SHOW" || cmd == "show")
+                {
+                    Console.WriteLine("Showing status...");
+                    Console.WriteLine($"Num = {num}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command");
+                }
+            }
+        }
+    }
+}
